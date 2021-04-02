1259c4a32b31dc477515babf2c74b636467ffdd7
JadeD99 <79310337+JadeD99@users.noreply.github.com>
2021-04-02 08:23:14 +0200

    aanvulling geocoder

--- script/main.js
+++ script/main.js
@@ -1,3 +1,38 @@
+// js voor de afteller
+
+var countDownDate = new Date("June 17, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("afteller").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("afteller").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+
+//map//
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiamFkZWQ5OSIsImEiOiJja21rbDhiMmcxMXB2MnZxazFnOWR2cmRtIn0.wJ7MX9l2Aj0xYOi6iYgSBw';
 
 var map = new mapboxgl.Map({
@@ -54,48 +89,6 @@ var marker4 = new mapboxgl.Marker({color: "#B1B9FC"})
 .addTo(map);
 
 
-
-
- 
-map.on('load', function () {
-// Add Mapillary sequence layer.
-// https://www.mapillary.com/developer/tiles-documentation/#sequence-layer
-map.addSource('mapillary', {
-'type': 'vector',
-'tiles': [
-'https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt'
-],
-'minzoom': 6,
-'maxzoom': 14
-});
-map.addLayer(
-{
-'id': 'mapillary',
-'type': 'line',
-'source': 'mapillary',
-'source-layer': 'mapillary-sequences',
-'layout': {
-'line-cap': 'round',
-'line-join': 'round'
-},
-'paint': {
-'line-opacity': 0.6,
-'line-color': '#8390FA',
-'line-width': 2
-}
-},
-'waterway-label'
-);
-});
- console.log(map);
-map.addControl(new mapboxgl.NavigationControl());
-
-// markers toevoegen
-// var popup = new mapboxgl.Popup({ offset: 25 }).setText(
-// 'Veel landingsplezier hier.'
-// );
-
-
 //------------Geocoder--------------
 var geocoder = new MapboxGeocoder({
 accessToken: mapboxgl.accessToken,
@@ -140,6 +133,64 @@ var degC = Math.floor(responseWeather.main.temp - 273.15);
 weatherBox.innerHTML = degC + '&#176;C <br>' + responseWeather.weather[0].description;
 });
 });
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+map.on('load', function () {
+// Add Mapillary sequence layer.
+// https://www.mapillary.com/developer/tiles-documentation/#sequence-layer
+map.addSource('mapillary', {
+'type': 'vector',
+'tiles': [
+'https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt'
+],
+'minzoom': 6,
+'maxzoom': 14
+});
+map.addLayer(
+{
+'id': 'mapillary',
+'type': 'line',
+'source': 'mapillary',
+'source-layer': 'mapillary-sequences',
+'layout': {
+'line-cap': 'round',
+'line-join': 'round'
+},
+'paint': {
+'line-opacity': 0.6,
+'line-color': '#8390FA',
+'line-width': 2
+}
+},
+'waterway-label'
+);
+});
+ console.log(map);
+map.addControl(new mapboxgl.NavigationControl());
+
+// markers toevoegen
+// var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+// 'Veel landingsplezier hier.'
+// );
+
+
+
 
 
 
